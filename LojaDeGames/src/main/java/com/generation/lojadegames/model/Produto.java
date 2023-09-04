d544baf278722eb67c353181fa63478f7a88cd0a
Thiago Reis <reiscorithi@gmail.com>
2023-09-04 20:20:47 -0300

    CRUD do banco de dados

---
+++ LojaDeGames/src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,78 @@
+package com.generation.lojadegames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é Obrigatorio")
+	@Size(max = 255, message = "O atributo nome deve conter no maximo 255 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo preço é Obrigatorio")
+	private float preco;
+	
+	@NotNull(message = "O atributo preço é Obrigatorio")
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}	
+	
+	
+}
